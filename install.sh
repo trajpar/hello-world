5fccfd68d226895af3c4709a96b72917639ffae6
trajpar <trajpar68@gmail.com>
2017-03-27 13:49:15 -0500

    Update install.sh
    Added install of youtube-dl and ffmpeg

--- install.sh
+++ install.sh
@@ -4,20 +4,29 @@
 
 sudo apt-get install unzip
 
-# Fetch and unpack
+# Fetch and unpack rclone
 
 curl -O http://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 cd rclone-*-linux-amd64
 
-# Copy binary file
+# Copy binary file rclone
 
 sudo cp rclone /usr/bin/
 sudo chown root:root /usr/bin/rclone
 sudo chmod 755 /usr/bin/rclone
 
-# Install manpage
+# Install manpage rclone
 
 sudo mkdir -p /usr/local/share/man/man1
 sudo cp rclone.1 /usr/local/share/man/man1/
 sudo mandb
+
+# Fetch and install youtube-dl
+
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# Install ffmpeg
+
+sudo apt install ffmpeg
